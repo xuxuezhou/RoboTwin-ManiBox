4b684c67f83d97115809ac6523df0e488b1a529c
Tian Nian (粘天) <115152418+Tian-Nian@users.noreply.github.com>
2025-06-06 17:53:27 +0800

    Update transforms.py

--- policy/openpi/src/openpi/transforms.py
+++ policy/openpi/src/openpi/transforms.py
@@ -300,16 +300,9 @@ class PromptFromLeRobotTask(DataTransformFn):
     tasks: dict[int, str]
 
     def __call__(self, data: DataDict) -> DataDict:
-        # if "task_index" not in data:
-        #     raise ValueError('Cannot extract prompt without "task_index"')
-
-        # task_index = int(data["task_index"])
-        # if (prompt := self.tasks.get(task_index)) is None:
-        #     raise ValueError(f"{task_index=} not found in task mapping: {self.tasks}")
         if "task" not in data:
             raise ValueError('Cannot extract prompt: "task" key not found in data')
         prompt = data["task"]
-
         return {**data, "prompt": prompt}
 
 def flatten_dict(tree: at.PyTree) -> dict:
