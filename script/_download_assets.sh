625ecea08f8014bfc973bbe895e34b4b897ee3bc
TianxingChen <chentianxing2002@gmail.com>
2025-06-27 09:31:17 +0800

    update download script

--- script/_download_assets.sh
+++ script/_download_assets.sh
@@ -1,2 +1,14 @@
 cd assets
 python _download.py
+
+# background_texture
+unzip background_texture.zip
+rm -rf background_texture.zip
+
+# embodiments
+unzip embodiments.zip
+rm -rf embodiments.zip
+
+# objects
+unzip objects.zip
+rm -rf objects.zip
